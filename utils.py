4b411184f8de73766e38927c23b1ae014c1e963f
Aibanez <aibanez@miuandes.cl>
2025-07-28 11:01:59 -0400

    update distributed method

--- utils.py
+++ utils.py
@@ -516,7 +516,7 @@ def init_distributed_mode(args):
         os.environ['MASTER_PORT'] = '29500'
 
     dist.init_process_group(
-        backend="gloo",  # gloo backend works for both CPU and GPU
+        backend="nccl",  # gloo backend works for both CPU and GPU
         init_method=args.dist_url,
         world_size=args.world_size,
         rank=args.rank,
